36d04d1e6a1e84fc4e2e66d3f6bb49a49edd9c35
Marcel Suter <marcel.suter@bzz.ch>
2023-08-28 10:22:14 +0200

    Update test_bicycle.py
    added underscores so the tests run without property/setters

--- test_bicycle.py
+++ test_bicycle.py
@@ -7,9 +7,9 @@ class TestBicycle:
 
     def test_initialization(self):
         bike = Bicycle()
-        assert bike.type == 'mountainbike'
-        assert bike.size == 48
-        assert bike.color == 'grey'
+        assert bike._type == 'mountainbike'
+        assert bike._size == 48
+        assert bike._color == 'grey'
 
     def test_type_set_get(self):
         bike = Bicycle()
